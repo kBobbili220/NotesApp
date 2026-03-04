902c4f77a35d60cb594e123d297f317ea63e6b4e
kBobbili220 <karthikk.bobbili@gmail.com>
2026-03-04 14:16:40 -0600

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic'
+    }
+
+    return (
+        <div style={footerStyle}>
+            <br />
+            <p>
+                Note app, Department of Computer Science, University of Bobbili
+            </p>
+        </div>
+    )
+}
+
+export default Footer
